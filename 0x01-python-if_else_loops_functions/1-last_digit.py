202dd3e80be64a35adc041b0af1afc898d4d3297
Paul Kirugu <pkirugu.chege111@gmail.com>
2023-08-10 14:32:53 +0000

    Task 1: print last digit commit 2

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,12 +3,15 @@ import random
 number = random.randint(-10000, 10000)
 # Extract the last digit of the number
 last_digit = abs(number) % 10
+# handles negative last digit if number id less than 0
+if number < 0:
+    last_digit = -last_digit
 # Determine the description based on the last digit
 if last_digit > 5:
     description = "greater than 5"
 elif last_digit == 0:
     description = "is zero"
 else:
-    description =f"less than 6 and not 0"
-# print the desired output    
+    description = f"less than 6 and not 0"
+# print the desired output
 print(f"Last digit of {number} is {last_digit} and is {description}")
