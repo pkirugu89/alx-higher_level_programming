da1bfb6616b7a3ac2b3f860be6df92b5740e5add
Paul Kirugu <pkirugu.chege111@gmail.com>
2023-08-11 14:00:31 +0000

    Task 1: Calculator commit 1

--- 0x02-python-import_modules/1-calculator.py
+++ 0x02-python-import_modules/1-calculator.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    a = 10
-    b = 5
     # import calculator-1
     from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+
     sum1 = add(a, b)
     sub1 = sub(a, b)
     mul1 = mul(a, b)
     div1 = div(a, b)
     # output the results
-    print(f"{a} + {b} = {sum1}")
-    print(f"{a} - {b} = {sub1}")
-    print(f"{a} * {b} = {mul1}")
-    print(f"{a} / {b} = {div1}")
+    print("{} + {} = {}".format(a, b, sum1))
+    print("{} - {} = {}".format(a, b, sub1))
+    print("{} * {} = {}".format(a, b, mul1))
+    print("{} / {} = {}".format(a, b, div1))
