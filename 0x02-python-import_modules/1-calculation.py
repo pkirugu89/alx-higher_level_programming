a940c4f2e8ac14d248b41ba7afc55c4c5ab8226b
Paul Kirugu <pkirugu.chege111@gmail.com>
2023-08-11 15:03:28 +0000

    Task 1: Calculator commit 6

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    # import calculator-1
+    from calculator_1 import add, sub, mul, div
+    # declaration of var a and b
+    a = 10
+    b = 5
 
-# import calculator-1
-from calculator_1 import add, sub, mul, div
-# declaration of var a and b
-a = 10
-b = 5
-
-# output the results
-print("{} + {} = {}".format(a, b, add(a, b)))
-print("{} - {} = {}".format(a, b, sub(a, b)))
-print("{} * {} = {}".format(a, b, mul(a, b)))
-print("{} / {} = {}".format(a, b, div(a, b)))
+    # output the results
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
