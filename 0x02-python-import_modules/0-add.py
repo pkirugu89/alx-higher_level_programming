f9999d6c1442a8ee2abeaeac9ce409da0d89df2c
Paul Kirugu <pkirugu.chege111@gmail.com>
2023-08-11 13:23:59 +0000

    Task 0: addition of a n b commit

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    # perform addition of a and b
+    result = add(a, b)
+    # output the result
+    print("{} + {} = {}".format(a, b, result))
