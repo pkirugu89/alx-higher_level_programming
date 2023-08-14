b43af121d5141d8c8b8741488d53b9e6eaa9c40a
Paul Kirugu <pkirugu.chege111@gmail.com>
2023-08-14 17:17:53 +0000

    Task 3: print integers in reverse commit 1

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    for num in reversed(my_list):
-        print("{}".format(num))
+    my_list_len = len(my_list)
+    my_list.reverse()
+    for num in range(my_list_len):
+        print("{:d}".format(my_list[num]))
