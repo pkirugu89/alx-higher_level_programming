b2ec6be08a43723d58637ba385b4ecf64e717966
Paul Kirugu <pkirugu.chege111@gmail.com>
2023-08-14 18:50:56 +0000

    Task 9: Find biggest number in list commit

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    # define length of my_list
+    list_len = len(my_list)
+    # Return None if the list is empty
+    if list_len == 0:
+        return None
+    # Initialize the maximum value with the first element of the list
+    max_val = my_list[0]
+    # Iterate through the list to find the maximum value
+    for num in range(list_len):
+        if my_list[num] > max_val:
+            max_val = my_list[num]
+
+            return max_val
