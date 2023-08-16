85f17ca2f270bd0d648682be3b96e46cd9a59b73
Paul Kirugu <pkirugu.chege111@gmail.com>
2023-08-16 17:34:48 +0000

    Task 7 and 8 update commit

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    # Sort the keys in alphabetic order
-    sort_keys = sorted(a_dictionary.keys())
-    # Iterate through the sorted keys and print the key-value pairs
-    for k in sort_keys:
-        value = a_dictionary[k]
-        print("{}: {}".format(k, value))
+    if a_dictionary is None:
+        print("Dictionary is None")
+        return
+    else:
+        # Sort the keys in alphabetic order
+        sort_keys = sorted(a_dictionary.keys())
+        # Iterate through the sorted keys and print the key-value pairs
+        for k in sort_keys:
+            value = a_dictionary[k]
+            print("{}: {}".format(k, value))
--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,3 +2,4 @@
 def update_dictionary(a_dictionary, key, value):
     # Update the value for the given key in the dictionary
     a_dictionary[key] = value
+    return a_dictionary
