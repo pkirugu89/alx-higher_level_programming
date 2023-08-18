f2c0e9b48fe10b2f14f84d98c201b34c443af320
Paul Kirugu <pkirugu.chege111@gmail.com>
2023-08-18 13:51:56 +0000

    add lists.h struct decription commit

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,6 +1,16 @@
 #ifndef LISTS_H
 #define LISTS_H
 #include <stddef.h>
+
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ *
+ */
 typedef struct dlistint_s
 {
 	int n;
