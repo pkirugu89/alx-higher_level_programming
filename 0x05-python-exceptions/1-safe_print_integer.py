b7c6dd3e6445386bcbcf302518f9ceb66cdf2621
Paul Kirugu <pkirugu.chege111@gmail.com>
2023-08-28 17:26:20 +0000

    Task 1: Safe print of int list commit

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    try:
+        # Format and print the integer value
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError):
+        return False
