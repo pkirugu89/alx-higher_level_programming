4da48d42655e453272686529b6986acdd0fedb8c
Paul Kirugu <pkirugu.chege111@gmail.com>
2023-08-28 17:17:46 +0000

    Task 0: print list commit 1

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i in range(x):
             # print the element at index i
-            print(my_list[i], end=" ")
+            print(my_list[i], end='')
             count += 1
     except IndexError:
         pass
