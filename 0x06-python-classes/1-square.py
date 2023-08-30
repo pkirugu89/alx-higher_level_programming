4ce66d5bb187881d13d60180092a0634526c57c0
Paul Kirugu <pkirugu.chege111@gmail.com>
2023-08-30 16:25:04 +0000

    task 1: square with size commit 2

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
 class Square:
+    """
+    This class defines a square.
+    A square is a 4- sided polygon with all sides of equal length
+        and all angles of 90 degrees
+        """
     def __init__(self, size):
+        """
+        Initialize a square with a given size.
+        :param size: sides of a square.
+        """
         self.__size = size
