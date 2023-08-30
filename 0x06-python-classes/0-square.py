b79d5b8227002d6b595886a591063eec3d08b157
Paul Kirugu <pkirugu.chege111@gmail.com>
2023-08-30 16:15:52 +0000

    Task 0: define class square commit 1

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 class Square:
     """
-    This class defines a square
+    This class defines a square.
+    A square is a four-sided polygon with all sides of equal length
+        and all angles of 90 degrees.
     """
     pass
