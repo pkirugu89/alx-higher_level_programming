c12528be0229f487df17b556be9693b0316f6524
Paul Kirugu <pkirugu.chege111@gmail.com>
2023-08-31 17:10:04 +0000

    4-square to 6-square commit

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,25 +1,56 @@
 #!/usr/bin/python3
+""" This class defines a square."""
+
+
 class Square:
     """ This class defines a Square. """
     def __init__(self, size=0):
-        """ initialize the size argument."""
+        """ initialize the size argument.
+        Args:
+        size (int): size of square.
+
+        Returns:
+        int: The size of square.
+        """
         self.__size = size
 
     # square area
     def area(self):
-        """Returns the square area."""
+        """Method that returns the square area.
+
+        Args:
+        self: local variable.
+
+        Returns:
+        int: square area.
+        """
         return self.__size**2
 
     # getter method
     @property
     def size(self):
-        """ returns the size of the square."""
+        """ Method returns the size of the square.
+
+        Args:
+        self: local variable.
+
+        Returns:
+        int: the size of square
+        """
         return self.__size
 
     # size setter method
     @size.setter
     def size(self, value):
-        """ size setter method."""
+        """ size setter method.
+
+        Args:
+        self: local variable.
+        value: variable that holds user input.
+
+        Returns:
+        int: square size.
+        """
         if type(value) != int:
             raise TypeError("size must be an integer")
         if value < 0:
--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""This class defines a Square."""
+
+
 class Square:
     """ This class defines a Square."""
     def __init__(self, size=0, position=(0, 0)):
