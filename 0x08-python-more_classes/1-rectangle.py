4763012feb8907a67ed5304cc6f128fc0de4ba30
Paul Kirugu <pkirugu.chege111@gmail.com>
2023-09-06 17:27:32 +0000

    1-rectangle update

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,8 +8,8 @@ class Rectangle:
     """
     def __init__(self, width=0, height=0):
         """ Rectangle class constructor."""
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     # Width getter method
     @property
