562e2548694ebf9d7b4e860bc2f2e3765ff2f00b
Paul Kirugu <pkirugu.chege111@gmail.com>
2023-09-12 19:45:30 +0000

    Task 6: BaseGeometry area() commit

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Class that defines a BaseGeometry."""
+
+
+class BaseGeometry:
+    """Class that defines BaseGeometry."""
+    def area(self):
+        """
+        Raises an exception message.
+        """
+        raise Exception("area() is not implemented")
