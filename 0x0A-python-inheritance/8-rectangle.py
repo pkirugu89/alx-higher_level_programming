fc2e7f85c82f3413a4f3f489b6ff2b3e39ada4ed
Paul Kirugu <pkirugu.chege111@gmail.com>
2023-09-13 17:34:29 +0000

    task 1 commit 2

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Class Rectangle."""
+
+
+class Rectangle(BaseGeometry):
+    """Class that defines a Rectangle."""
+    def __init__(self, width, height):
+        """
+        Initialize a Rectangle instance with width and height.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+
+        Raises:
+            ValueError: If width or height is not a positive integer.
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def __str__(self):
+        """__str__ magic method."""
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+    def area(self):
+        """Calculate and return the area of the rectangle."""
+        return self.__width * self.__height
