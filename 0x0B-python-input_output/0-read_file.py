fff81d3c0c96352f894cedc687845e699a063d7b
Paul Kirugu <pkirugu.chege111@gmail.com>
2023-09-16 08:57:41 +0000

    Task 0: read file commit 1

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""Read file method."""
+
+
 def read_file(filename=""):
     """Function that reads a textfile and prints it."""
-
-    with open(filename) as f:
+    with open(filename, encoding="UTF-8") as f:
         text = f.read()
         print(text, end="")
