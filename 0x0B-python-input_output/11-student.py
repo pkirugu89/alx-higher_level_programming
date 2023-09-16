e96d72cc56a09ba3cb22d921575ce24bd44e8627
Paul Kirugu <pkirugu.chege111@gmail.com>
2023-09-16 12:19:28 +0000

    Task 11: Student to disk and reload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Class that define a Student."""
+
+
+class Student:
+    """Class that represent a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialize a new Student.
+
+        Args:
+            firstname (str): student's first name.
+            lastname (str): student's last name.
+            age (int): student's age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Student info in JSON format.
+        Args:
+            attrs (list): Thr attributes to represent.
+        """
+        if (type(attrs) == list and
+                all(type(el) == str for el in attrs)):
+            return {x: getattr(self, x) for x in attrs if hasattr(self, x)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """
+        Replace all student attributes.
+        Args:
+            json (dict): Key/value pairs to replace attribute with.
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
