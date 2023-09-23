bbc7531131f8ba917017f85e9764f1e99a38a802
Paul Kirugu <pkirugu.chege111@gmail.com>
2023-09-23 14:50:04 +0000

    Task 8: update 0 commit

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -101,3 +101,21 @@ class Rectangle(Base):
         """
         return "[Rectangle] ({}) {}/{} - {}/{}"\
             .format(self.id, self.x, self.y, self.width, self.height)
+
+    def update(self, *args):
+        """
+        Update attributes with the provided arguments in the specified order.
+        Args:
+            *args: Arguments in the order (id, width, height, x, y).
+        """
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.width = args[1]
+            if len(args) >= 3:
+                self.height = args[2]
+            if len(args) >= 4:
+                self.x = args[3]
+            if len(args) >= 5:
+                self.y = args[4]
