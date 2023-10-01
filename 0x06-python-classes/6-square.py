a487081686068093af631e78ee2e83a423378622
Paul Kirugu <pkirugu.chege111@gmail.com>
2023-10-01 18:36:30 +0000

    task 6 update 2.1 commit

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -75,11 +75,15 @@ class Square:
         """
         if self.__size == 0:
             print()
-        else:
-            for _ in range(self.__position[1]):
-                print()
-            for _ in range(self.__size):
-                print(" " * self.__position[0] + "#" * self.__size)
+            return
+        for y in range(0, self.__position[1]):
+            print()
+        for i in range(0, self.__size):
+            for x in range(0, self.__position[0]):
+                print(" ", end="")
+            for j in range(0, self.__size):
+                print("#", end="")
+            print()
 
     # position getter method
     @property
