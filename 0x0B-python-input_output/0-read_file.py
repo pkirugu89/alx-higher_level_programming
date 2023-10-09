32eefdcbdb81483bf54fcf413930e251462e18be
Paul Kirugu <111176482+pkirugu89@users.noreply.github.com>
2023-10-09 08:50:59 +0300

    Delete 0x0B-python-input_output directory
    deferred to cohort 17

--- 0x0B-python-input_output/0-read_file.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Read file method."""
-
-
-def read_file(filename=""):
-    """Function that reads a textfile and prints it."""
-    with open(filename, encoding="UTF-8") as f:
-        text = f.read()
-        print(text, end="")
