5974f7354c283ffc87dc6a91f80dc6e12bc0bc26
Paul Kirugu <pkirugu.chege111@gmail.com>
2023-10-30 07:09:02 +0000

    Task 4: Print Square commit

---
+++ 0x07-python_driven_development/4-print_square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Print Square Method. """
+
+
+def print_square(size):
+    # Check if size is an integer
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    # Check if size is non-negative
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    # Check if size is a float and is less than 0
+    if isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+    # Print the square
+    for _ in range(size):
+        print("#" * size)
