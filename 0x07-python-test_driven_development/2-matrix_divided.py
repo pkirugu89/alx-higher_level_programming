7b2687ffe3c4c768acd88acd2d293d5f0898d042
Paul Kirugu <pkirugu.chege111@gmail.com>
2023-11-04 16:03:49 +0000

    matrix update

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -3,20 +3,36 @@
 
 
 def matrix_divided(matrix, div):
+    """
+    Method that returns a new matrix.
+    The result of the matrix division by div
+    rounded to 2 decimal places.
+    """
     # Check if the matrix is a list of integers or floats
-    if not all(isinstance(x, list) and
-            all(isinstance(el, (int, float)) for el in x) for x in matrix):
+    if not isinstance(matrix, list) or len(matrix) == 0 \
+            or not matrix[0]:
         raise TypeError("matrix must be a matrix (list of lists)\
                 of integers/floats")
-    # Check if each row of matrix is of the same size
-    if not all(len(rw) == len(matrix[0]) for rw in matrix):
+    for rw in matrix:
+        if len(rw) == 0:
+            raise TypeError("matrix must be a matrix (list of lists)\
+                    of integers/floats")
+        for r in rw:
+            if type(r) is not int and type(r) is not float:
+                raise TypeError("matrix must be a matrix (list of lists)\
+                        of integers/floats")
+
+    row_len = []
+    for row in matrix:
+        row_len.append(len(row))
+    if not all(el == row_len[0] for el in row_len):
         raise TypeError("Each row of the matrix must have the same size")
-    # Check if the div is a number (integer / float)
-    if not isinstance(div, (int, float)):
+
+    if type(div) is not int and type(div) is not float:
         raise TypeError("div must be a number")
-    # Check if div is not equal to zero
+
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    # Divide all matrix elements by div and round to 2 decimal places
-    results = [[round(y / div, 2) for y in row] for row in matrix]
-    return results
+
+    nw_matrix = [[round(x / div, 2) for x in row] for row in matrix]
+    return nw_matrix
