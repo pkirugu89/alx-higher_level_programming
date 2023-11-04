b8ff4046c9fd7159652ec55db7cb8fc9c437497e
Paul Kirugu <pkirugu.chege111@gmail.com>
2023-11-04 17:39:36 +0000

    Text indentation update

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -4,15 +4,27 @@
 
 def text_indentation(text):
     """
-    Method that returns formatted text indented.
+    Method that indent text and split it
+    Args:
+        text (str): text to be indented.
+    Returns:
+        str: returns a formatted string.
     """
     # check if text is a string
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     # Split the text based on ".", "?", and ":"
-    split_text = text.split(".") + text.split("?") + text.split(":")
+    split_text = text.split(".")
+    temp_text = []
+    for item in split_text:
+        temp_text.extend(item.split("?"))
+    final_text = []
+    for item in temp_text:
+        final_text.extend(item.split(":"))
+
     # Remove empty strings and strip leading/trailing whitespace
-    split_text = [line.strip() for line in split_text if line.strip() != ""]
+    split_text = [line.strip() for line in final_text if line.strip() != ""]
+
     # Print the formatted text
     formatted_text = "\n\n".join(split_text)
     print(formatted_text)
