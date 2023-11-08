4aa735c6d72dd0ae15a603c016f958d1af16ef8c
Paul Kirugu <pkirugu.chege111@gmail.com>
2023-11-08 18:36:12 +0000

    Task 9: rectangle area commit

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Class that defines a Rectangle..
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Class that defines Rectangle.
+    """
+    def __init__(self, width, height):
+        """
+        Initialize a Rectangle instance with width and height.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+
+        Returns:
+            ValueError: If width or height is not a positive integer.
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """
+        Method that returns the area of the rectangle.
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """
+        Method that returns the string rep of the rectangle.
+        """
+        return f"[Rectangle] {self.__width}/{self.__height}"
