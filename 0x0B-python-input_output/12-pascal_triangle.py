5ac3941b7ea70933063ea92006244eff9aa9b8fc
Paul Kirugu <pkirugu.chege111@gmail.com>
2023-11-09 18:49:55 +0000

    Task 12: Pascal triangle commit

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Function that defines Pascal's triangle.
+"""
+
+
+def pascal_triangle(n):
+    """
+    Method that returns a list of int triangle of size n.
+    """
+    if n <= 0:
+        return []
+
+    triangles = [[1]]
+    while len(triangles) != n:
+        tri = triangles[-1]
+        tmp = [1]
+        for i in range(len(tri) - 1):
+            tmp.append(tri[i] + tri[i + 1])
+        tmp.append(1)
+        triangles.append(tmp)
+    return triangles
