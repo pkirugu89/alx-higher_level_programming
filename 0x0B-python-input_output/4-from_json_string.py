326818fb8482f005629d96b688bc5fd7458a094c
Paul Kirugu <pkirugu.chege111@gmail.com>
2023-11-09 17:06:06 +0000

    Task 4: from json str to obj commit

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Function that converts JSON to string object.
+"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    Function that converts JSON to string.
+    Args:
+        my_str (str): JSON file to be converted.
+    Returns:
+        str: returns python version of a JSON str.
+    """
+    return json.loads(my_str)
