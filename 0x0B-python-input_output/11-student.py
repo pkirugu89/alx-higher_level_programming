c9741e89cd0d06772c8cd57ea637cc245e868029
Paul Kirugu <pkirugu.chege111@gmail.com>
2023-11-09 18:36:07 +0000

    Task 11: reload from json file

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Class that defines a Student.
+"""
+
+
+class Student:
+    """
+    Class that represent a student.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialize a new student.
+        Args:
+            first_name (str): student's first name.
+            last_name (str): student's last name.
+            age (int): student's age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Student info in JSON format.
+        Args:
+            attrs (list): The attributes to represent.
+        """
+        if (type(attrs) == list and
+                all(type(el) == str for el in attrs)):
+            return {x: getattr(self, x) for x in attrs if hasattr(self, x)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """
+        Replace all student attributes.
+        Args:
+            json (dict): Key/value pairs to replace attribute with.
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
