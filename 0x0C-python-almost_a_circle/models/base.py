f0a9fd0e9523b32df7f4d2b41c7a5d4a3bf2bab3
Paul Kirugu <pkirugu.chege111@gmail.com>
2023-11-13 15:41:49 +0000

    Task 15: json string rep commit

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,8 @@
 """
 Class that defines a Base.
 """
+import json
+import os
 
 
 class Base:
@@ -24,3 +26,16 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        Converts dict list to a JSON string rep.
+        Args:
+            list_dictionaries (list): dict list converted into JSON.
+        Returns:
+            str: JSON string rep of dict list.
+        """
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
