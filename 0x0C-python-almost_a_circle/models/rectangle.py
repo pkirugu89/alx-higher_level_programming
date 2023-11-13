76625cbc3d8bbcbd62675daea1afc5eb8e1c8b95
Paul Kirugu <pkirugu.chege111@gmail.com>
2023-11-13 14:21:02 +0000

    Task 5 and task 6 commit

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -129,3 +129,10 @@ class Rectangle (Base):
             [print(" ", end="") for x in range(self.x)]
             [print("#", end="") for w in range(self.width)]
             print("")
+
+    def __str__(self):
+        """
+        Return the print() and str() rectangle representation.
+        """
+        return "[Rectangle] ({}) {}/{} - {}/{}"\
+            .format(self.id, self.x, self.y, self.width, self.height)
