36d008f530d5bb1296f002707bc0b73393bb3fc0
Paul Kirugu <pkirugu.chege111@gmail.com>
2023-11-13 16:07:19 +0000

    json save to file and read commit

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -39,3 +39,31 @@ class Base:
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        Method that writes json str rep of list_objs to a file.
+        Args:
+            list_objs (list): List instances that inherits from Base.
+        """
+        file_name = cls.__name__ + ".json"
+        with open(file_name, mode="w") as file:
+            if list_objs is None:
+                file.write("[]")
+            else:
+                list_dicts = [ob.to_dictionary() for ob in list_objs]
+                file.write(cls.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        Converts JSON str to dict list.
+        Args:
+            json_sring (str): JSON sring rep.
+        Returns:
+        list: Dicts list represented by json_string.
+        """
+        if json_string is None or len(json_string) == 0:
+            return []
+        return json.loads(json_string)
