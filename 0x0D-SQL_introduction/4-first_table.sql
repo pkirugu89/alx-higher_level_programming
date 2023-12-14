27ccff2012b0f4a449d793fc33d302d61185b301
Paul Kirugu <pkirugu.chege111@gmail.com>
2023-12-14 11:46:20 +0000

    Task 4 and 8 commit

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,4 +1,3 @@
 -- Script that creates a table first_table in hbtn_0c_0 database
-USE hbtn_0c_0
 CREATE TABLE IF NOT EXISTS first_table (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256));
 
---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,3 @@
+-- script that counts the number of values in a table
+SELECT COUNT(*) FROM first_table WHERE id = 89;
+
