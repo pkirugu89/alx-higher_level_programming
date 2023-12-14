d7b36d42ba75335730ba7cd2bf2812aedbb10a50
Paul Kirugu <pkirugu.chege111@gmail.com>
2023-12-14 11:59:27 +0000

    Task 9: full create table commit

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,15 @@
+-- script that creates a second table into hbtn_0c_0 db and add 3 records.
+-- create second_table if it doesn't exist.
+CREATE TABLE IF NOT EXISTS second_table (
+	id INT,
+	name VARCHAR(256),
+	score INT
+);
+
+-- insert 3 records into the second_table
+INSERT INTO second_table (id, name, score) VALUES
+(1, 'John', 10),
+(2, 'ALex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
+
