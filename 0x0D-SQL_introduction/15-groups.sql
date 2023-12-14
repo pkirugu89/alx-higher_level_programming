c03a5ffee1cf63be124f9582c020ddd7e6c3d4ab
Paul Kirugu <pkirugu.chege111@gmail.com>
2023-12-14 12:49:37 +0000

    Task 15: number by score commit

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,6 @@
+-- script that lists number of records with the same score in second_table
+SELECT score, COUNT(*) AS number 
+FROM second_table 
+GROUP BY score 
+ORDER BY number DESC, score DESC;
+
