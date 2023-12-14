dd6a2f531f851fa33b6708036a567c37183437e7
Paul Kirugu <pkirugu.chege111@gmail.com>
2023-12-14 09:24:58 +0000

    Task 4: first table commit

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,4 @@
+-- Script that creates a table first_table in hbtn_0c_0 database
+USE hbtn_0c_0
+CREATE TABLE IF NOT EXISTS first_table (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256));
+
