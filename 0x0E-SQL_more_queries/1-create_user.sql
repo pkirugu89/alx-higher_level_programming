c0464d77ef3586bdfd77fe35469365bfdcad02b2
Paul Kirugu <pkirugu.chege111@gmail.com>
2023-12-16 10:03:09 +0000

    Task 1: create user commit

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- script that creates user_0d_1 and grant the user all privileges
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
