5d8428c3ca195e9c6b2e7b3675db10a2e567f435
Paul Kirugu <pkirugu.chege111@gmail.com>
2023-12-16 11:33:14 +0000

    Task 8: cities in California commit

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,6 @@
+-- script that lists all cities in Califonia state.
+SELECT cities.id, cities.name 
+FROM cities, states 
+WHERE cities.state_id = states.id AND states.name = 'California'
+ORDER BY cities.id ASC;
+
