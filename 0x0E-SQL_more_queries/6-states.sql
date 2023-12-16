10ebd7bc43755312162d17f62a7473d0f9ee664f
Paul Kirugu <pkirugu.chege111@gmail.com>
2023-12-16 11:10:47 +0000

    Task 6: state table commit

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,10 @@
+-- creates the database 'hbtn_0d_usa' if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- use the hbtn_0d_usa
+USE hbtn_0d_usa;
+-- create table 'states' if it doesn't exist
+CREATE TABLE IF NOT EXISTS states (
+	id INT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY,
+	name VARCHAR(256) NOT NULL
+);
+
