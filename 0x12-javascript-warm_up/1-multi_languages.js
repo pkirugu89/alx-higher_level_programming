c2a9a833d660740967eefcb8d83408984c894e3c
Paul Kirugu <pkirugu.chege111@gmail.com>
2024-01-09 17:48:32 +0000

    Task 1: 3 languages commit

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// Printing the 1st line
+console.log('C is fun');
+// Printing the 2nd line
+console.log('Python is cool');
+// Printing the 3rd line
+console.log('JavaScript is amazing');
