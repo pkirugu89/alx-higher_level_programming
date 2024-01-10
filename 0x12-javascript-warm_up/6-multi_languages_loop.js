133c7dcb692ad54b524b42f44c93727cee0ada2c
Paul Kirugu <pkirugu.chege111@gmail.com>
2024-01-10 16:04:56 +0000

    Task 6: Loop to languages commit

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// String array
+const lang = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+// Looping through the array and print each line
+for (let x = 0; x < lang.length; x++) {
+  console.log(lang[x]);
+}
