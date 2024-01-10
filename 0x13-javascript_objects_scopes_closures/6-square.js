6ccf42e710f845e334c5419441b4135478aa433c
Paul Kirugu <pkirugu.chege111@gmail.com>
2024-01-10 20:25:40 +0000

    Task 6: Square part 1 commit

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+// Require the Square class from 5-square.js file
+const Square5 = require('./5-square');
+
+// Define the Square class that inherits from Rectangle class.
+class Square extends Square5 {
+  // class cosntructor
+  constructor (size) {
+  // call the super class cosntructor
+    super(size);
+  }
+
+  // Instance method for printing the square using character 'c'
+  charPrint (c) {
+  // If c is undefined, use character 'X'
+    if (c === undefined) {
+      c = 'X';
+    }
+
+    // Loop the square print using char 'c'
+    for (let x = 0; x < this.height; x++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+// Make the Square class visible
+module.exports = Square;
