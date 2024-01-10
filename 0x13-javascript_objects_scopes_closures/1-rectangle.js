e33aceafa2ec841c98a56546c9390dcf39891195
Paul Kirugu <pkirugu.chege111@gmail.com>
2024-01-10 18:03:37 +0000

    Task 1: Rectangle constructor commit

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// Define the Rectangle class
+class Rectangle {
+  // class constructor
+  constructor (w, h) {
+    // initialize instance width n height instance
+    this.width = w;
+    this.height = h;
+  }
+}
+// Make the Rectangle class visible
+module.exports = Rectangle;
