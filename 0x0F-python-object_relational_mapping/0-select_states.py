fd5e7da5e2aab46f8c67737b9215ee90c0c771d4
Paul Kirugu <pkirugu.chege111@gmail.com>
2024-01-14 08:26:43 +0000

    Task 0: Get all states commit

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Script that lists all states from hbtn_0e_0_usa db.
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # check if the correct number of args is provided
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+
+    # extract arguments
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+    # connect to the MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        user=username,
+        passwd=password,
+        db=database,
+        port=3306
+    )
+    # Create a cursor obj to interact with the db
+    cursor = db.cursor()
+    # Execute the SQL query to select states ordered by id
+    dbquery = "SELECT * FROM states ORDER BY id ASC;"
+    cursor.execute(dbquery)
+
+    # Fetch all rows from the result set
+    rows = cursor.fetchall()
+
+    # Display the results
+    for row in rows:
+        print(row)
+
+    # close the cursor and db connection
+    cursor.close()
+    db.close()
