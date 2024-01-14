6d3e2b4a3eb582f4d76f684c7af937dfa506390f
Paul Kirugu <pkirugu.chege111@gmail.com>
2024-01-14 11:16:50 +0000

    Task 7: All states via SQLAlchemy commit

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Script that lists all State objects from the database hbtn_0e_6_usa
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+if __name__ == "__main__":
+    # Check if the correct number of arguments is provided
+    if len(sys.argv) != 4:
+        print("\
+                Usage: {} <username> <password> \
+                <database>".format(sys.argv[0]))
+        sys.exit(1)
+
+    # Extract arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Connect to the MySQL server
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        username,
+        password,
+        database),
+        pool_pre_ping=True
+        )
+
+    # Create a session
+    session = Session(engine)
+
+    # Fetch all State objects and display their details
+    states = session.query(State).order_by(State.id).all()
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+
+    # Close the session
+    session.close()
