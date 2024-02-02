f2911a1129b874d95a5bc860f0842a970c4dba54
Paul Kirugu <pkirugu.chege111@gmail.com>
2024-02-02 11:51:47 +0000

    Task 2: POST email msg commit

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Send POST email. """
+import urllib.request
+import urllib.parse
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: ./2-post_email.py <URL> <email>")
+        sys.exit(1)
+
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+
+    with urllib.request.urlopen(url, data=data) as response:
+        content = response.read().decode('utf-8')
+        print(content)
