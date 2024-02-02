4aeb56736480263dd1e2d272b8ae5669fa05c1b0
Paul Kirugu <pkirugu.chege111@gmail.com>
2024-02-02 11:27:04 +0000

    Task 0: what's my status commit 1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
         content = response.read()
 
     print("Body response:")
-    print(f"    - type: {type(content)}")
-    print(f"    - content: {content}")
-    print(f"    - utf8 content: {content.decode('utf-8')}")
+    print(f"- type: {type(content)}")
+    print(f"- content: {content}")
+    print(f"- utf8 content: {content.decode('utf-8')}")
