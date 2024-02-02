58d42e3e409615c07eb3e45aaf8d16fec93ad588
Paul Kirugu <pkirugu.chege111@gmail.com>
2024-02-02 12:02:27 +0000

    Task 4: What's my status #1 commit

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import requests
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+
+    response = requests.get(url)
+    content = response.text
+
+    print("Body response:")
+    print(f"\t- type: {type(content)}")
+    print(f"\t- content: {content}")
