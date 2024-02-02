b735b394fb100a8cd30cfcd370b0a9c1430b924e
Paul Kirugu <pkirugu.chege111@gmail.com>
2024-02-02 08:07:23 +0000

    task 1 and 2 update commit

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes a URL, sends a GET request, and displays the body of a 200 status code response
-curl -s "$1" -o /dev/null -w "%{http_code}" | grep -q ^200 && curl -s "$1"
+curl -sL "$1"
