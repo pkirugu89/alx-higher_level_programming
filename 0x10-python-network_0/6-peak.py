28dd5f222f460533f47bc268d533f421a1f88784
Paul Kirugu <pkirugu.chege111@gmail.com>
2024-02-02 08:50:31 +0000

    Task 6: find a peak commit

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+def find_peak(list_of_integers):
+    """
+    Finds the peak in a list of unsorted integers
+    Args:
+        list_of_integers: List of unsorted integers.
+    Returns:
+        The value of a peak in the list, or None if the list is empty.
+    """
+    if not list_of_integers:
+        return None
+
+    low = 0
+    high = len(list_of_integers) - 1
+
+    while low < high:
+        mid = (low + high) // 2
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            low = mid + 1
+        else:
+            high = mid
+
+    # High points to a peak at the end of loop
+    return list_of_integers[high]
