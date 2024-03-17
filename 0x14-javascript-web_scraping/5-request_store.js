a3fa82c1a9c16b705d7d9f8f1a9b01700151d414
Paul Kirugu <pkirugu.chege111@gmail.com>
2024-03-17 11:42:35 +0300

    Task 5: Loripsum commit 1

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -23,9 +23,9 @@ function writeFilez (filePath, content) {
   fs.writeFile(filePath, content, 'utf8', (error) => {
     if (error) {
       console.error('Error writing to file: ', error);
-      return;
+    //  return;
     }
-    console.log('Webpage Content successfully stored in: ', filePath);
+    // console.log('Webpage Content successfully stored in: ', filePath);
   });
 }
 displayWebPage(url, filePath);
